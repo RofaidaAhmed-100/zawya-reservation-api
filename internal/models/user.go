165b792dc51a287784c5df40ea6b6ee448ea9afb
Rofaida Ahmed <rofaidaahmed879@gmail.com>
2025-12-08 23:22:51 +0200

    init

---
+++ internal/models/user.go
@@ -0,0 +1,22 @@
+package models
+
+import (
+	"time"
+	"gorm.io/gorm"
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID        string    `gorm:"type:char(36);primaryKey" json:"id"`
+	Email     string    `gorm:"uniqueIndex;not null" json:"email"`
+	Password  string    `gorm:"not null" json:"-"`
+	Name      string    `gorm:"not null" json:"name"`
+	Role      string    `gorm:"type:varchar(20);default:'user'" json:"role"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func (u *User) BeforeCreate(tx *gorm.DB) error {
+	u.ID = uuid.New().String()
+	return nil
+}
